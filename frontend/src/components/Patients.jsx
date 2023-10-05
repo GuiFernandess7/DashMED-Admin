29b5d159ff4be95b57d04499fc3644bff189bd40
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-10-04 21:07:15 -0300

    Packages updated

--- frontend/src/components/Patients.jsx
+++ frontend/src/components/Patients.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import {Box} from "@mui/material";
 import Header from "./Header"
-import toast, { Toaster } from 'react-hot-toast'
-//import { FontAwesomeIcon } from "@fontawesome/react-fontawesome"
-//import { faTrash, faUpload } from "@fontawesome/free-solid-svg-icons"
 import { useState } from 'react'
 
 import useSWR from 'swr'
@@ -30,11 +27,13 @@ const Patients = () => {
         <Box display="flex" justifyContent="space-between" alignItems="center">
             <Header title="PACIENTES" subtitle="Acesso a lista de pacientes"/>
         </Box>
-        <div>{patients.map((patient)=> (
-          <div className='patient' key={patient.id}>
-            <h2>{patient.name} - {patient.id}</h2>
-          </div>
-        ))}</div>
+        <div>
+            {patients && patients.map((patient) => (
+            <div className='patient' key={patient.id}>
+              <h2>{patient.name} - {patient.id}</h2>
+            </div>
+        ))}
+        </div>
     </Box>
   )
 }
